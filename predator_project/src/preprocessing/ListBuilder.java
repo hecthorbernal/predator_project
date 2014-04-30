961dd8250b8985d6c5b71b0b402293f6d812e139
jacobbj0311 <jaje@itu.dk>
2014-04-30 18:07:22 +0200

    minor makeup:-)

--- predator_project/src/preprocessing/ListBuilder.java
+++ predator_project/src/preprocessing/ListBuilder.java
@@ -10,110 +10,71 @@ import java.util.List;
 
 public class ListBuilder {
 
-	String[] attributeList = new String[7];
+	/**
+	 * Builds list from en_us.blacklists.txt
+	 */
+	public static void main(String[] args) {
 
-	public ListBuilder() {
-
-		attributeList[0] = "word";
-		attributeList[1] = "matchWholeWord";
-		attributeList[2] = "isYoutube";
-		attributeList[3] = "isUsername";
-		attributeList[4] = "isAlert";
-		attributeList[5] = "isBlackList";
-		attributeList[6] = "isExactFiltering";
-
-	}
-
-	public void buildList() {
 
 		String file = "data/en_us.blacklist.csv";
-		int numberOfFields = 7;
-
-		ArrayList<ArrayList<String>> newLists = new ArrayList<ArrayList<String>>();
-
-		for (int i = 0; i < numberOfFields; i++) {
-
-			newLists.add(i, new ArrayList<String>());
-
-		}
-
 		FileInputStream fis;
-		
+
+		String[] attributeList;
+		ArrayList<ArrayList<String>> newLists;
+
 		try {
 			fis = new FileInputStream(file); 
 			//Construct BufferedReader from InputStreamReader
 			BufferedReader br = new BufferedReader(new InputStreamReader(fis));
 			String line = null;
-			int i = 0;
 
-			// skip first line
+			// build attributelist from first line
 			line = br.readLine();
+			attributeList = line.split(",");
 
+			// prepare lists
+			newLists = new ArrayList<ArrayList<String>>();
+
+			for (int i = 0; i < attributeList.length; i++)
+				newLists.add(i, new ArrayList<String>());
+
+			// build lists
 			while ((line = br.readLine()) != null) {
 
-
 				//Split line and build when column true
-
 				String[] split = line.split(",");
 
-				for(int j=1; j < numberOfFields ; j++) {
-
-					if(split[j].equals("1")) {
-
-						// add text to list
+				for(int j=1; j < attributeList.length; j++) 
+					if(split[j].equals("1")) 
 						newLists.get(j-1).add(split[0]);
-
-					}	
-
-				}
-
 			}
 
 
 			br.close();
+
+			for (int i = 1; i < attributeList.length; i++) {
+
+				// save list to file
+				String filename = "data/blacklists/en_us." + attributeList[i] + ".txt";	
+				saveListToFile(newLists.get(i-1), filename);	
+
+			}
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
-		for (int i = 1; i < numberOfFields; i++) {
-
-			// save list to file
-
-			String filename = "data/blacklists/en_us." + this.attributeList[i] + ".txt";
-			saveListToFile(newLists.get(i-1), filename);	
-
-		}
-
 	}
 
+	private static void saveListToFile(List<String> list, String sFileName){
 
+		try {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		ListBuilder myList = new ListBuilder();
-
-		myList.buildList();
-
-	}
-
-	private static void saveListToFile(List<String> list, String sFileName)
-	{
-		try
-
-		{
 			FileWriter writer = new FileWriter(sFileName);
 
 			// add lines
-			for(String s: list) {
-
-				writer.append(s);
-				writer.append('\n');
-			}
+			for(String s: list)
+				writer.append(s + "\n");
 
 			writer.flush();
 			writer.close();
@@ -128,5 +89,6 @@ public class ListBuilder {
 		System.out.println("Exported file: " + sFileName);
 
 	}
+
 
 }
